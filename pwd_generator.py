dae0843b82627c216b29d1c1e02836a7e90cab43
Chip <Panopticon772@gmail.com>
2020-06-28 18:21:30 -0400

    Added random capital letters.

--- pwd_generator.py
+++ pwd_generator.py
@@ -1,13 +1,16 @@
 import random
 
-units = [
+symbols = [
     '!',
     '@',
     '#',
     '$',
     '%',
     '&',
-    '*',
+    '*'
+]
+
+numbers = [
     0,
     1,
     2,
@@ -17,19 +20,22 @@ units = [
     6,
     7,
     8,
-    9,
+    9
+]
+
+letters = [
     'a',
     'b',
     'c',
     'd',
     'e',
-    'f'
+    'f',
     'g',
     'h',
     'i',
     'j',
     'k',
-    'l'
+    'l',
     'm',
     'n',
     'o',
@@ -47,11 +53,39 @@ units = [
 ]
 
 
+def random_letter():
+    case = random.randint(0, 1)
+    if case == 0:
+        return random.choice(letters).capitalize()
+    else:
+        return random.choice(letters)
+
+
+def random_symbol():
+    return random.choice(symbols)
+
+
+def random_number():
+    return random.choice(numbers)
+
+
 def generate_pwd(pwd_length):
     pwd_arr = []
     i = 0
     while i < pwd_length:
-        pwd_arr.append(random.choice(units))
+        random_character = random.randint(0, 2)
+        # if 0, returns random letter
+        if random_character == 0:
+            print('randomn letter:', random_letter())
+            pwd_arr.append(random_letter())
+        # if 1, returns random symbol
+        elif random_character == 1:
+            print('randomn symbol:', random_symbol())
+            pwd_arr.append(random_symbol())
+        # if 2, returns random number
+        else:
+            print('randomn number:', random_number())
+            pwd_arr.append(random_number())
         i += 1
     pwd = ''.join(map(str, pwd_arr))
     print(f'Your new password is: {pwd}')
@@ -61,9 +95,9 @@ create_pwd = True
 
 while create_pwd == True:
     pwd_length = int(input(
-        'Please enter the length of the password you desire. Must be a number between 8 and 12. '
+        'Please enter the length of the password you desire. Must be a number between 8 and 16. '
     ))
-    if pwd_length >= 8 and pwd_length <= 12:
+    if pwd_length >= 8 and pwd_length <= 16:
         generate_pwd(pwd_length)
         create_pwd = False
     else:
